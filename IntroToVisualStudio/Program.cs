faeace18ebe56bbdc2f4f2177fefe33789a7d96b
Vazha Nikabadze <v.Nikabadze@tgm.loc>
2023-03-02 14:29:24 +0400

    Add new VS solution

---
+++ IntroToVisualStudio/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace IntroToVisualStudio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Some Fuck'in text");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.ReadLine();
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine("Why are you so Serious");
+          
+        }
+    }
+}
+
+
